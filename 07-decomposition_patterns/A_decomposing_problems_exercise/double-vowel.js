eeddc1e3f6c161fe9bfabdacb0b9b16ee27e14fd
Mzwandile <mzwandilethatojunior@gmail.com>
2023-04-16 17:03:09 +0200

    ninth commit

---
+++ 07-decomposition_patterns/A_decomposing_problems_exercise/double-vowel.js
@@ -0,0 +1,21 @@
+// Write a function `doubleVowel` that accepts a string as an argument. The function should return
+// the string where every occurrence of a vowel in the original string is repeated twice consecutively.
+// Vowels are the letters a, e, i, o, u.
+
+function doubleVowel(str){
+    let str2 = '';
+
+    for(i=0; i<str.length; i++){
+        if(str[i]==='a'  ||str[i]==='e'  ||str[i]==='i'  ||str[i]==='o'  ||str[i]==='u'){
+            str2+=str[i]+str[i];
+        }else{
+            str2+=str[i];
+        }
+    }
+
+    return str2;
+}
+
+console.log(doubleVowel("runner")); // 'ruunneer'
+console.log(doubleVowel("stoplight")); // 'stoopliight'
+console.log(doubleVowel("gardener")); // 'gaardeeneer'
---
+++ 07-decomposition_patterns/A_decomposing_problems_exercise/is-prime.js
@@ -0,0 +1,31 @@
+// Write a function `isPrime` that accepts a number as an argument. The function should return a
+// boolean indicating whether or not the given number is prime. A prime number is a number that is
+// only divisible by 1 and itself. The smallest prime number is 1.
+//
+// For example, 11 is a prime because it is only divisible by 1 and 11.
+// For example, 8 is not a prime because it is divisible by 1, 2, 4, and 8
+
+function isPrime(num){
+    if (num === 1){
+        return false;
+    }else if(num === 2){
+        return true;
+    }else{
+        for(i=2; i<num; i++){
+            if(num % i === 0){
+                return false;
+            }
+        }
+        if(i===num){
+            return true;
+        }
+    }
+}
+
+console.log(isPrime(11)); // true
+console.log(isPrime(8)); // false
+console.log(isPrime(7)); // true
+console.log(isPrime(21)); // false
+console.log(isPrime(2)); // true
+console.log(isPrime(15)); // false
+console.log(isPrime(1)); // false
