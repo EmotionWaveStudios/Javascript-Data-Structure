66fb6283ba7e34055c9d878596f8c94ce3481536
Mzwandile <mzwandilethatojunior@gmail.com>
2023-07-02 22:30:16 +0200

    project update

--- 07-decomposition_patterns/B_decomposing_problems_exercise/most-letter-word.js
+++ 07-decomposition_patterns/B_decomposing_problems_exercise/most-letter-word.js
@@ -3,32 +3,7 @@
 // of times. You can assume that the sentence contains at least one word. If there is a tie, return
 // the word that appears earlier in the sentence.
 
-function mostLetterWord(str, char){
-    let obj = {};
-    let arr = str.split(' ');
-    let num = 0;
-    let word = '';
 
-    for(let element in arr){
-        obj[arr[element]] = 0;
-    }
-
-    for(let element in obj){
-        for(i=0; i<element.length; i++){
-            if(element[i]===char){
-                obj[element]++;
-            }
-        }
-    }
-
-    for(let element in obj){
-        if(obj[element]>num){
-            num = obj[element];
-            word = element;
-        }
-    }
-    return word;
-}
 
 console.log(mostLetterWord('she received an award for excellence in science', 'e')); // 'excellence'
 console.log(mostLetterWord('she received an award for excellence in science', 'a')); // 'award'
--- 07-decomposition_patterns/B_decomposing_problems_exercise/pick-prefix.js
+++ 07-decomposition_patterns/B_decomposing_problems_exercise/pick-prefix.js
@@ -1,16 +1,7 @@
 // Write a function `pickPrefix(strings, prefix)` that accepts an array of strings and a prefix string 
 // as an argument. The function should return the an array of words that begin with the prefix.
 
-function pickPrefix(string, prefix){
-    let arr = [];
 
-    for(i=0; i<string.length; i++){
-        if(string[i].indexOf(prefix)===0){
-            arr.push(string[i]);
-        }
-    }
-    return arr;
-}
 
 console.log(pickPrefix(['connect', 'company', 'concert', 'cram'], 'con'));
 // ['connect', 'concert']
